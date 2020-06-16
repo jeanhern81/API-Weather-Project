208812e74b24623f82040fe00c6396f55ed66ae4
jeanhern81 <jeanhern81@gmail.com>
2020-06-16 16:10:31 -0700

    updated variables on js doc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-let cityLat = 0;
-let cityLon = 0;
-let cityName = "";
-let countryCode = "";
+$(document).ready(function () {
+    let appID = "2d52799f575e2efb8c494bcd1971726e";
+    let weather = "";
+    let city = "";
+    let current_date = moment().format("L");
+    let search_history = JSON.parse(localStorage.getItem("cities")) === null ? [] : JSON.parse(localStorage.getItem("cities"));
+
